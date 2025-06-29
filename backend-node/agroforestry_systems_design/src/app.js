c818c94b4951bf4f88bc90c07242e6ae7a0c4b3a
LeandroSmS <leandrosimoesms@gmail.com>
2025-06-29 07:58:03 -0300

    Feat/backend-node: Added populate bd data

--- backend-node/agroforestry_systems_design/src/app.js
+++ backend-node/agroforestry_systems_design/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import canteiroRoutes from './routes/canteiro.js';
 import sequelize from './config/db.js';
+import { populateDb } from './config/populateDb.js';
 
 const app = express();
 app.use(cors());
@@ -9,8 +10,9 @@ app.use(express.json());
 
 app.use('/', canteiroRoutes);
 
-sequelize.sync().then(() => {
+sequelize.sync().then(async () => {
   console.log('Banco de dados sincronizado');
+  await populateDb();
   app.listen(5002, () => {
     console.log('API Agroforestry rodando em http://localhost:5002');
   });
