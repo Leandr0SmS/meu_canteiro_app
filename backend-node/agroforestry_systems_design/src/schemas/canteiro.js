a7a4a7275b47b37eff44c88c678f454eb5c01453
Leandro Sms <leandrosimoesms@gmail.com>
2025-07-03 16:38:42 -0300

    feat/backend: Removed python backend and change name of node to backend

--- backend-node/agroforestry_systems_design/src/schemas/canteiro.js
+++
@@ -1,9 +0,0 @@
-export function apresentaCanteiro(canteiro) {
-  return {
-    id_canteiro: canteiro.id_canteiro,
-    nome_canteiro: canteiro.nome_canteiro,
-    x_canteiro: canteiro.x_canteiro,
-    y_canteiro: canteiro.y_canteiro,
-    plantas_canteiro: canteiro.plantas_canteiro
-  };
-}
--- backend/agroforestry_systems_design/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-from schemas.canteiro import CanteiroSchema, CanteiroBuscaSchema, CanteiroViewSchema, apresenta_canteiro, \
-                            CanteiroUpdateSchema, CanteiroDelSchema, ListagemCanteirosSchema, apresenta_canteiros, \
-                            BuscaCanteiroIdSchema, apresenta_canteiro_destribuido, CanteiroSchemaDestribuido, \
-                            PlantasCanteiroSchema
-
-                            
-from schemas.error import ErrorSchema
